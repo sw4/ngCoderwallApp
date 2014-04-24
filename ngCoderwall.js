e799b5535b505e08b1df138a5318cd55b5eb9cbd
SW4 <samuel.wheeler@gmail.com>
2014-04-24 11:18:11 +0100

    update 0.0.1

---
+++ ngCoderwall.js
@@ -0,0 +1,33 @@
+var ngCoderwall = angular.module('ngCoderwall', []);
+
+ngCoderwall.controller("CoderwallListController", ['$scope', 'CoderwallFactory', '$timeout', function ($scope, CoderwallFactory, $timeout) {
+
+    $scope.badges=[];
+    $scope.getBadges= function(username){
+
+        CoderwallFactory.fetch(username).then(function (response) {        
+
+$scope.badges=response.data.data.badges;
+console.log(response.data.data.badges);
+       //     $timeout(function(){
+        //        $scope.answers=answers;
+       //     });                
+
+        });
+
+    }    
+
+}]);
+ngCoderwall.factory('CoderwallFactory', ['$http', '$timeout', function ($http, $timeout) {
+    var factory = {
+        fetch : function (username) {
+            return $http.jsonp('https://coderwall.com/'+username+'.json?callback=JSON_CALLBACK').success(function (data) {
+                return data;
+            }).error(function (e) {
+                return e;
+            });
+        }
+    };
+    return factory;   
+
+}]);
